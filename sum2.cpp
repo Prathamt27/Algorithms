99012e2abae9bef1c0c6c36c3b68619b2358657c
Prathamt27 <113412787+Prathamt27@users.noreply.github.com>
2022-10-07 13:43:17 +0530

    sum of two numbers

---
+++ sum2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b,sum;
+    cout<<"Enter postitive first number: ";
+    cin>>a;
+    cout<<"Enter positive second number: ";
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum of two numbers is: ";
+    cout<<sum;
+    return 0;
+    
+}
